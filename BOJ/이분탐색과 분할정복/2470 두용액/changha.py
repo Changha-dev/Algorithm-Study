6e7de3aa8930ac9ba6645780a47bd41c72ebde20
changha <mentenseoul@gmail.com>
2024-05-03 17:38:33 +0900

    create BOJ-2470-두 용액

--- BOJ/이분탐색과 분할정복/2470 두용액/changha.py
+++ BOJ/이분탐색과 분할정복/2470 두용액/changha.py
@@ -1,3 +1,28 @@
-# [-99, -4, -2, -1, 98]
-# [1, 3, 5, 7]
-# 
+n = int(input())
+li = list(map(int, input().split()))
+
+li.sort()
+
+start = 0
+end = len(li)-1
+
+prev = float('inf') 
+res = (0, 0)
+
+while start < end:
+    cur = (li[start] + li[end])
+    mid = (start + end) // 2
+
+    if abs(cur) < abs(prev):
+        prev = cur
+        # 이때 결과 저장
+        res = (li[start], li[end])
+
+    if cur < 0:
+        start += 1
+    elif cur > 0:
+        end -= 1
+    else:
+        break
+
+print(res[0], res[1])
